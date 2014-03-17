0b18096d00f856b0fd61c0277aec69c45ba32167
Janosch Machowinski <janosch.machowinski@dfki.de>
2014-03-17 17:30:52 +0100

    Added StateAggregator Class
    This is a helper class to aggregate states in motor drivers.

---
+++ src/StateAggregator.cpp
@@ -0,0 +1,153 @@
+#include "StateAggregator.hpp"
+#include <iostream>
+#include <math.h>
+
+namespace joint_dispatcher
+{
+    
+StateAggregator::StateAggregator(const std::vector< std::pair<size_t, std::string> >& actuatorMap, boost::function<void (const base::commands::Joints &status)> writeStatusCallback, base::Time statusInterval) : 
+    actuatorCnt(0), updateCnt(0), statusInterval(statusInterval), writeStatusCallback(writeStatusCallback)
+{
+    actuatorCnt = actuatorMap.size();
+    status.resize(actuatorCnt);
+
+    for (size_t i = 0; i < actuatorCnt; ++i)
+    {
+        size_t board_idx = abs(actuatorMap[i].first);
+        
+        StateInfo info;
+        info.updated = false;
+        info.stateId  = board_idx;
+        info.outputPosition = i;
+        
+        stateMap.insert(std::make_pair(board_idx, info));
+        
+        if(stateIdToInfo.size() <= board_idx)
+        {
+            stateIdToInfo.resize(board_idx + 1, NULL);
+        }
+        stateIdToInfo[board_idx] = &(stateMap[board_idx]);
+    }
+
+    reset();
+}
+
+const std::vector<size_t> StateAggregator::getActuatorIds() const
+{
+    std::vector<size_t> ret;
+    for(std::map<size_t, StateInfo>::const_iterator it = stateMap.begin(); it != stateMap.end(); it++)
+    {
+        ret.push_back(it->second.stateId);
+    }
+    return ret;
+}
+
+
+void StateAggregator::reset()
+{
+    writeState = false;
+    
+    //reinit the update counter
+    updateCnt = actuatorCnt;
+    
+    //reset the updated flag
+    for(std::map<size_t, StateInfo>::iterator it = stateMap.begin(); it != stateMap.end(); it++)
+    {
+        it->second.updated = false;
+    }
+}
+
+
+void StateAggregator::setNewStatus(size_t stateId, const base::Time stateTime, const base::JointState& state)
+{
+    
+    StateInfo &info(*(stateIdToInfo[stateId]));
+    
+    if(status.time > stateTime)
+    {
+        std::cout << "Warning, got state update for actuator from the past " << status.time << " " << stateTime << std::endl;
+    }
+    
+    //if the time of the samples differ by more than an interval 
+    //than the stateSet is missaligned in time.
+    //if info.updated is set we lost a sample.
+    //Anyway, we delay the write of the status 
+    //and hope that the rest of the status will 
+    //arrive before the call to checkWriteState
+    if(info.updated || (stateTime - status.time > statusInterval))
+    {
+        writeState |= true;
+    }
+
+    if(!info.updated)
+        //nominal case
+        updateCnt--;
+    
+    info.updated = true;
+    info.updateTime = stateTime;
+    status[stateId] = state;
+
+    //allways use the latest time
+    status.time = stateTime;
+
+    //if we got updates for all joints,
+    //this means we have a full and valid state,
+    //allways send it out directly
+    if(updateCnt <= 0)
+    {
+        writeState = true;
+        checkWriteState();
+    }
+}
+
+void StateAggregator::checkWriteState()
+{
+    if(writeState)
+    {
+        writeStatusCallback(status);
+        reset();
+    }
+}
+
+
+CommandDispatcher::CommandDispatcher(const std::vector< int32_t >& actuatorMap, boost::function<void (int32_t actuatorId, base::JointState::MODE mode, double value)> setCommandCallback): setCommandCallback(setCommandCallback)
+{
+    for(uint32_t i = 0; i < actuatorMap.size();i++)
+    {
+        int board_idx = abs(actuatorMap[i]) - 1;
+        //zero has the special meaning
+        //of 'do not use this id'
+        if(actuatorMap[i] == 0)
+        {
+            continue;
+        }
+        InputMapping m;
+        m.actuatorId = board_idx;
+        m.inputId = i;
+        
+        inputMap.push_back(m);
+    }   
+}
+
+const std::vector<int32_t> CommandDispatcher::getActuatorIds() const
+{
+    std::vector<int32_t> ret;
+    for(std::vector<InputMapping>::const_iterator it = inputMap.begin(); it != inputMap.end(); it++)
+    {
+        ret.push_back(it->actuatorId);
+    }
+    return ret;
+
+}
+
+void CommandDispatcher::processCommand(base::samples::Joints cmd)
+{
+    for(std::vector<InputMapping>::const_iterator it = inputMap.begin(); it != inputMap.end(); it++)
+    {
+        const int i = it->inputId;
+        
+        setCommandCallback(it->actuatorId, cmd[i].getMode(), cmd[i].getField(cmd[i].getMode()));
+    }
+};
+
+}
---
+++ src/StateAggregator.hpp
@@ -0,0 +1,99 @@
+#ifndef STATEAGGREGATOR_HPP
+#define STATEAGGREGATOR_HPP
+#include <vector>
+#include <map>
+#include <base/time.h>
+#include <base/commands/Joints.hpp>
+#include <boost/function.hpp>
+
+namespace joint_dispatcher
+{
+
+class StateAggregator
+{
+    class StateInfo
+    {
+    public:
+        size_t stateId;
+        size_t outputPosition;
+        bool updated;
+        base::Time updateTime;
+    };
+    
+    ///amount of dispatched actuators
+    size_t actuatorCnt;
+    
+    ///conter for arrived state updates
+    size_t updateCnt;
+    
+    base::commands::Joints status;
+    std::vector<StateInfo *> stateIdToInfo;
+    
+    std::map<size_t, StateInfo> stateMap;
+
+    ///The nominal time between two state updates
+    base::Time statusInterval;
+    
+    boost::function<void (const base::commands::Joints &status)> writeStatusCallback;
+    
+    bool writeState;
+protected:
+    const base::commands::Joints &getStatus() const 
+    {
+        return status;
+    };
+    
+public:
+    
+    const std::vector<size_t> getActuatorIds() const;
+    
+    StateAggregator(const std::vector< std::pair<size_t, std::string> >& stateMap, boost::function<void (const base::commands::Joints &status)> writeStatusCallback, base::Time statusInterval);
+    ~StateAggregator() {};
+    
+    /**
+     * Updates a single status in the aggregator.
+     * This function also triggers the write of teh full state.
+     * */
+    void setNewStatus(size_t stateId, const base::Time stateTime, const base::JointState& state);
+
+    /**
+     * Checks if we got a full state and writes it
+     * using the writeStatusCallback function.
+     * Should be called regulary
+     * */
+    void checkWriteState();
+    
+    
+    /**
+    * Resets the aggregator state while keeping
+    * the id mapping and the names in the state.
+    * */
+    void reset();
+};
+
+class CommandDispatcher
+{
+    class InputMapping
+    {
+    public:
+        int actuatorId;
+        int inputId;
+    };    
+    
+    ///map from positions in the base::actuator::command to the driver ids
+    std::vector<InputMapping> inputMap;
+    
+    boost::function<void (int32_t actuatorId, base::JointState::MODE mode, double value)> setCommandCallback;
+    
+public:
+    const std::vector<int32_t> getActuatorIds() const;
+    
+    CommandDispatcher(std::vector< boost::int32_t > const & actuatorMap, boost::function<void (int32_t actuatorId, base::JointState::MODE mode, double value)> setCommandCallback);
+    virtual ~CommandDispatcher() {};
+    
+    void processCommand(base::commands::Joints cmd);    
+};
+
+}
+
+#endif // STATEAGGREGATOR_HPP
